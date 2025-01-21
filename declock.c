77dcc0666e9de3a4e6fb473f1b30a79994951fce
David Richards <rawdod@gmail.com>
2025-01-21 04:25:16 +0000

    test declock.c file

---
+++ declock.c
@@ -0,0 +1,24 @@
+/*C declock
+ *
+ * a different clock
+ *
+ * Jan 20, 2025 23:03 EST
+ *
+ * HI OK we are making a new kind of clock here, why for, special purposes.
+ * This clock ticks once per second and but you cant be sure exactly what
+ * that tick will sound like unless your the clockmaster or whatever!
+ * The value is stored in the lower 26 bits of a 4 byte ping packet data.
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  for(;;) { printf("trololololol\n"); }	
+  return 1;
+}
+
+
+
+
+
