070ea4e3ad24fa1b11a054dcdf7d988170d3b88b
David Richards <rawdod@gmail.com>
2025-02-16 10:00:45 +0000

    the system size is 2080 subsystems

--- math/munesyszgen.py
+++ math/munesyszgen.py
@@ -17,3 +17,19 @@ print(f"total bytes = {map_pam_sz + pam_hdr_sz} (this many)")
 print(f"kby te  u")
 
 print(f"mutool draw -o /demo/SSYSIMAP.pam -F pam /demo/lush/doc/pdf/munemap.pdf")
+
+
+1024 * 4  = 4096
+4096 * 26 = 106496
+4096 * 80 = 327680
+327680 * 26 = 8519680
+
+
+screen byte size 3145728  (1024x768 RGBA)
+
+screens 26 * 80 = 2080
+
+2080 * 3145728 = 6543114240
+
+6543114240 + 73 = 6543114313
+
